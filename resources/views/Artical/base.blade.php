d1608ceb104c05139e3293ba782deae143433f52
Lix <xiao.liunit@gmail.com>
2017-04-28 07:30:07 +0800

    nav

--- resources/views/Artical/base.blade.php
+++ resources/views/Artical/base.blade.php
@@ -6,20 +6,16 @@
     <link rel="stylesheet" type="text/css" href="{{ asset('bootstrap/dist/css/bootstrap.css') }}">
 </head>
 <body>
+    <nav class="navbar navbar-default navbar-fixed-top navbar-inverse">
+      <div class="container">
+        导航条
+      </div>
+    </nav>
     <div class="jumbotron">
-        <div class="container">
-          <h1>Hello, world!</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+        <div class="container-fluid">
+          <h1>Leon's Blog</h1>
+          <p>Management Systems</p>
         </div>
-    </div>
-    <div class="page-header">
-      <h1>Example page header <small>Subtext for header</small></h1>
     </div>
     <div class="content"></div>
     <footer></footer>
