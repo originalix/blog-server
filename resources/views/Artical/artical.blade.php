d83e9a45d032fbe62aa7ab7a7a08ad76b6e991e5
Lix <xiao.liunit@gmail.com>
2017-04-28 16:29:10 +0800

    artical

--- resources/views/Artical/artical.blade.php
+++ resources/views/Artical/artical.blade.php
@@ -3,11 +3,23 @@
 @section('title', 'Write')
 
 @section('content')
-    <div class="title">
-        <label class="col-sm-2 control-label">Title</label>
-        <div class="col-sm-10">
-        <input type="text" class="form-control" placeholder="文章标题">
-        </div>
-    </div>
+    <form>
+      <div class="title">
+          <div class="row">
+            <div class="col-md-8">
+              <input type="text" class="form-control" placeholder="文章标题">
+            </div>
+            <div class="col-md-4">
+             <input type="date" class="form-control" name="date">
+            </div>
+          </div>
+      </div>
+      <div class="artical">
+        <textarea class="form-control" rows="30"></textarea>
+      </div>
+      <div class="submit">
+        <button type="button" class="btn btn-primary">发布</button>
+      </div>
+    </form>
 @endsection
 
