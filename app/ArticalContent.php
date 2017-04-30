c4cf640132df4646d26ee3348958117ec018fe7a
Lix <xiao.liunit@gmail.com>
2017-04-30 22:43:33 +0800

    articalcontent model

--- app/ArticalContent.php
+++ app/ArticalContent.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class ArticalContent extends Model
 {
-    //
+    protected $table = 'ArticlesContent';
+    protected $primaryKey = 'id';
+    public $timestamps = true;
+    protected $guarded = [];
 }
