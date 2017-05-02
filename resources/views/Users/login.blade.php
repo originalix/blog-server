a3ccb9fc5a42ab8566b33aae53b6f179bd35f694
Lix <xiao.liunit@gmail.com>
2017-05-02 08:07:24 +0800

    login

--- resources/views/Users/login.blade.php
+++ resources/views/Users/login.blade.php
@@ -3,5 +3,33 @@
 @section('title', '登录')
 
 @section('content')
-登录界面
+<form class="form-horizontal" method="post" enctype="multipart/form-data">
+  {{ csrf_field() }}
+  <div class="form-group">
+    <label for="inputEmail3" class="col-sm-2 control-label">Username</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" name="username" id="inputEmail3" placeholder="Username">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control" name="password" id="inputPassword3" placeholder="Password">
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+      <div class="checkbox">
+        <label>
+          <input type="checkbox"> Remember me
+        </label>
+      </div>
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+      <button type="submit" class="btn btn-default">Sign in</button>
+    </div>
+  </div>
+</form>
 @endsection
