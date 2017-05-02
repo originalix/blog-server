90841af30758df75101a572a25676d79214df12f
Lix <xiao.liunit@gmail.com>
2017-05-02 12:10:45 +0800

    login valid

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,8 +18,32 @@ class UserController extends Controller
         dd($user);
     }
 
-    public function login()
+    public function login(Request $request)
     {
+        if ($request->isMethod('POST')) {
+            if ($this->validUserInfo($request)) {
+                return;
+            }
+            $username = $request->get('username');
+            $password = $request->get('password');
+            
+        }
         return view('Users.login');
+    }
+
+    private function validUserInfo(Request $request)
+    {
+        $username = $request->get('username');
+        $password = $request->get('password');
+        if ($username == null || $password == null) {
+            return false;
+        }
+        if (!preg_match('/^[a-za-z0-9_]{5,16}$/', $username)) {
+            return false;
+        }
+        if (!preg_match('/^[\\~!@#$%^&*()-_=+|{}\[\],.?\/:;\'\"\d\w]{5,16}$/', $password)) {
+            return false;
+        }
+        return true;
     }
 }
