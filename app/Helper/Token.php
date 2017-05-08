175ed986185344e985383713ab0cbd0bfdefcb48
Lix <xiao.liunit@gmail.com>
2017-05-09 07:12:41 +0800

    desc

--- app/Helper/Token.php
+++ app/Helper/Token.php
@@ -4,6 +4,12 @@ namespace App\Helper;
 
 class Token
 {
+    /** token 生成器
+     * @param $userId 用户id
+     * @param $mobile 手机号
+     * @param $createTime 创建时间
+     * @return string token
+     */
     static public function createToken($userId, $mobile, $createTime)
     {
         return hash("sha256", $userId . $mobile . $createTime);
