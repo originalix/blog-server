56191a447987db42d8af4a7117de19a317e8e041
Lix <xiao.liunit@gmail.com>
2017-05-14 10:24:42 +0800

    ajax

--- resources/views/Test/ajax.blade.php
+++ resources/views/Test/ajax.blade.php
@@ -1,40 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Leon's Blog - @yield('title')</title>
-    <link rel="stylesheet" type="text/css" href="{{ asset('bootstrap/dist/css/bootstrap.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/base.css') }}">
-    <script src="datetime/jquery/jquery-1.8.3.min.js"></script>
-</head>
-<body>
-    <nav class="navbar navbar-default navbar-fixed-top navbar-inverse">
-      <div class="container">
-        <div class="logo">Originalee</div>
-        <ul class="navi">
-          <li class="item active"><a href="login">Home</a></li>
-          <li class="item"><a href="artical">Write</a></li>
-        </ul>
-      </div>
-    </nav>
-    <div class="jumbotron">
-        <div class="container-fluid">
-          <h1>Leon's Blog</h1>
-          <p>Management Systems</p>
-        </div>
-    </div>
-    <div class="content">
-      <div class="container">
-        @section('content')
-          this is the master content
-        @show
-      </div>
-    </div>
-    <footer class="footer-wrapper">
-      <div class="sperate">
-        <hr>
-      </div>
-      <p>© 2015 - 2017 Originalee,powered By Leon</p>
-    </footer>
-</body>
-</html>
+@extends('Test.base')
+
+@section('content')
+    <h1>Ajax Ajax</h1>
+@stop
