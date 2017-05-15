6ceadb9b159df0a5c51930d251d80dd7c00fdbf6
Lix <xiao.liunit@gmail.com>
2017-05-15 16:06:30 +0800

    model test

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -2,10 +2,12 @@
 
 namespace Tests\Feature;
 
+use App\Artical;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+
 
 class UserTest extends TestCase
 {
@@ -31,5 +33,12 @@ class UserTest extends TestCase
         $y = 6;
         $add = $x + $y;
         $this->assertEquals($add === 11, $x + $y, '测试不正确');
+    }
+
+    public function testArtical()
+    {
+        $article = Artical::where('id', '>',  1)->first();
+        var_dump($article);
+        $this->assertTrue(!is_null($article));
     }
 }
