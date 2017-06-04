5e0324d896b9cb0fc928012961e0ca6b671470b1
Lix <xiao.liunit@gmail.com>
2017-06-04 20:31:20 +0800

    Books model

--- app/Books.php
+++ app/Books.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Books extends Model
 {
-    //
+    protected $table = 'books';
+    protected $primaryKey = 'id';
+    public $timestamps = true;
+    protected $guarded = [];
 }
